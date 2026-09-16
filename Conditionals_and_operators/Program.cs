f840d35421208edd70da2f71a525d4b79707b442
Gutz1913 <andresfgutierrezv@gmail.com>
2026-09-16 18:15:37 -0500

    Add new example for conditionals and operators

---
+++ Conditionals_and_operators/Program.cs
@@ -0,0 +1,18 @@
+﻿/* === Conditionals === */
+// Console.Write("Ingrese un número: ");
+// var num = int.Parse(Console.ReadLine());
+// if (num > 0) Console.WriteLine("El número es positivo.");
+// else if (num < 0) Console.WriteLine("El número es negativo.");
+// else Console.WriteLine("El número es cero.");
+/* === End Conditionals === */
+
+/* === Operators === */
+Console.Write("Ingrese el primer número: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo número: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"suma: {a + b}, Resta: {a - b}, Multiplicación: {a * b}, División: {(b!=0 ? a / b : "Indeterminado")}");
+
+/* === End Operators === */
+
