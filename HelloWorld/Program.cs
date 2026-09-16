9196ec6b5bd3c15d46c71194be177fb050c2dc6f
Gutz1913 <andresfgutierrezv@gmail.com>
2026-09-16 16:11:09 -0500

    Changehello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Name = "Andrés";
+            Console.WriteLine($"Hello World! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
